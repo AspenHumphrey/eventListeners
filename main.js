678d672250749ccdf1b01241c3615bbc3e156d1a
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-07 15:13:31 -0500

    finished up js in eventlisteners

--- main.js
+++ main.js
@@ -24,35 +24,26 @@ document.getElementById("add-color").addEventListener("click", function(){
 	console.log(piggie, piggie.classList);
 });
 
-let biggie = document.getElementById("make-large");
-
 document.getElementById("make-large").addEventListener("click", function(){
-	biggie.classList.toggle("big");
-	console.log(biggie, biggie.classList);
-});
-
-
-
-
-
-
-
-
-document.getElementById("add-color").addEventListener("click", function(){
-	piggie.classList.toggle("blue");
+	piggie.classList.toggle("bold");
 	console.log(piggie, piggie.classList);
 });
-document.getElementById("add-color").addEventListener("click", function(){
-	piggie.classList.toggle("blue");
-	console.log(piggie, piggie.classList);
-});
+
+// document.getElementById("add-color").addEventListener("click", function(){
+// 	piggie.classList.toggle("border");
+// 	console.log(piggie, ciggie.classList);
+// });
+
+// document.getElementById("add-color").addEventListener("click", function(){
+// 	piggie.classList.toggle("blue");
+// 	console.log(piggie, piggie.classList);
+// });
 
 
 
 
 
 console.log("still going");
-
 
 
 
