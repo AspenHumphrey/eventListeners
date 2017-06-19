9633539b7b9186346ab9400cdf8ad7b2f0ce9e3f
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-19 10:46:24 -0500

    append child

--- append-child.js
+++ append-child.js
@@ -1,7 +1,12 @@
 let myArr = ["Hello", "Howdy", "Bonjuor", "S'up?"];
-let phraseDiv = document.getElementById("greeting");
+let phraseDOM = document.getElementById("greeting");
 
-myArr.forEach( function(greeting) {
-	let phraseString = `<div><h3>${greeting}, world!</h3></div>`
-	phraseDiv.innerHTML += phraseDiv;
+myArr.forEach( function(greeting, index) {
+	let phraseString = `<div id="phrase-${index +1}"><h3>${greeting}, world!</h3></div>`
+	let phraseDiv = document.createElement("div");
+	phraseDiv.innerHTML += phraseString;
+	phraseDOM.appendChild(phraseDiv);
+	document.getElementById(`phrase-${index +1}`).addEventListener("click", function() {
+		event.target.classList.add("blue");
+	});
 });
